df4c1ee83939b6a3a121e02cb343b895ffa50d0f
Ismael Serrano <iserrano@visualms.com>
2021-05-26 20:12:42 +0200

    Instalamos Material UI y preparamos los componentes iniciales

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,14 @@
 import { useEffect, useState } from "react";
-import Status from "./components/Status";
+import SignIn from "./components/Sign/SignIn";
+import SignUp from "./components/Sign/SignUp";
 
-// Componente principal de la aplicación.
 const App = () => {
-  const [status, setStatus] = useState(false);
-  const [loading, setLoading] = useState(true);
+  useEffect(() => {}, []);
 
-  // Cargamos el estado del servidor
-  useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setStatus(data.status === "ok"))
-      .finally(() => setLoading(false));
-  }, []);
-
-  // Mostramos la aplicación
   return (
     <main>
-      <h1>Curso de React de TrainingIT</h1>
-      <p>
-        Estado del servidor:
-        {loading ? " Cargando..." : <Status status={status} />}
-      </p>
+      <SignUp />
+      <SignIn />
     </main>
   );
 };
